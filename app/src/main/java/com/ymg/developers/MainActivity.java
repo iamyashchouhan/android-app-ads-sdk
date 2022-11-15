56909c149e81f7ce73d41371d58aed32fd1d3442
iamyashchouhan <iamyashchouhan@gmail.com>
2022-11-15 19:05:21 +0530

    Updated

--- app/src/main/java/com/ymg/developers/MainActivity.java
+++ app/src/main/java/com/ymg/developers/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.ymg.library.format.BannerAd;
+
 public class MainActivity extends AppCompatActivity {
     
 
